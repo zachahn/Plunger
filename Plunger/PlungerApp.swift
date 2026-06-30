4b8308a5651406fcad847678d42baab784e1c1aa
Zach Ahn <engineering@zachahn.com>
2026-06-30 08:46:16 -0400

    Initial Commit

---
+++ Plunger/PlungerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlungerApp.swift
+//  Plunger
+//
+//  Created by Zach Ahn on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct PlungerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
